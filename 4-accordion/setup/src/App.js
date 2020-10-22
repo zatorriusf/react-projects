0dbea42965dc97ff37b9bc75c1e037e9fe7fa306
zatorriusf <zatorrius.furniss@outlook.com>
2020-10-22 12:03:55 -0400

    completed challenge 4

--- 4-accordion/setup/src/App.js
+++ 4-accordion/setup/src/App.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <main>
+      <div className="container">
+        <h3>Login FAQs</h3>
+        <section className="info">
+          {data.map(question =>{
+            return (<SingleQuestion {...question} key={question.id}/>)
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ id, info, title }) => {
+  const [viewInfo, setViewInfo] = useState(false);
+  return (
+    <article className="question">
+      <header>
+  {title} <button className="btn" onClick={() => {setViewInfo(!viewInfo)}}>{viewInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}</button>
+      </header>
+      {viewInfo && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
