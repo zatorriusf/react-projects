9a987c57ffeed75b8738dba8f19560b5b262cea9
zatorriusf <zatorrius.furniss@outlook.com>
2020-10-22 18:37:00 -0400

    completed lorem challenge

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,10 +1,29 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
+  const [paragraphsRequested, setParagraphsRequested] = useState(0);
+  const [amountValue, setAmountValue] = useState(0);
+  const handleSubmit = (evt) =>{
+    evt.preventDefault();
+    setParagraphsRequested(amountValue);
+  }
   return (
-  <h2>lorem ipsum project setup</h2>
+  <section className="section-center">
+    <h3>Making a fake ipsum</h3>
+    <form className='lorem-form' onSubmit={handleSubmit}>
+      <label htmlFor="amount">Paragraphs:</label>
+      <input type="number" name="amount" min="1" max={data.length.toString()} value={amountValue} onChange={evt => {setAmountValue(evt.target.value)}}/>
+      <button className="btn" onClick={handleSubmit}>Generage</button>
+    </form>
+    {paragraphsRequested > 0 && <article className="lorem-text">
+    {
+      data.slice(0,paragraphsRequested).map((text,index) =>{
+        return <p key={index}>{text}</p>
+      })
+    }
+  </article>}
+  </section>
+  
     )
 }
 
