0bd9e0b93a84b6258cd2e97b066bc07c3bd508f3
zatorriusf <zatorrius.furniss@outlook.com>
2020-10-26 09:31:14 -0400

    completed navbar challenge

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,56 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  const [showLinksContainer, setShowLinksContainer] = useState(false);
+  const linkContainerRef = useRef();
+  const linksRef = useRef();
 
-export default Navbar
+  useEffect(()=>{
+    const linkHeight = linksRef.current.getBoundingClientRect().height;
+    if(showLinksContainer){
+      linkContainerRef.current.style.height=`${linkHeight}px`
+    }
+    if(!showLinksContainer){
+      linkContainerRef.current.style.height=`0px`
+    }
+  }
+  ,[showLinksContainer])
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+          <button className="nav-toggle" onClick={()=>setShowLinksContainer(!showLinksContainer)}>
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container" ref={linkContainerRef}>
+          <ul className="links" ref={linksRef}>
+            {links.map(link => {
+              return <li key={link.id}>
+                <a href={link.url}>
+                  {link.text}
+                </a>
+              </li>
+            })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map(link =>{
+            return <li key={link.id}>
+              <a href={link.url}>
+                {link.icon}
+              </a>
+            </li>
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
